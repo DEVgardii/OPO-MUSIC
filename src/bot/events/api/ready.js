fb55b01bef9c15a652893127618ac0293f70ab45
zhanyarbot12 <93198999+zhanyarbot12@users.noreply.github.com>
2021-12-28 14:23:38 +0300

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -5,6 +5,7 @@ module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
 
-    await client.user.setActivity(`Server ${client.guilds.cache.size} - User ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} | p >>> name mwsic`);
+    await client.user.setActivity(`Server ${client.guilds.cache.size} - User ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} | p >>> name music`);
+await client.user.setActivity(`By : TOP + STAFF = GARDI ᵖᵏ#1111 `);
     await client.user.setStatus("idle");
 }
